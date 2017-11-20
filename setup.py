b6690016e4ae965d7c5f8332ec65d11109c1002f
levan <levantsinadze@gmail.com>
2017-11-20 19:29:58 +0400

    setup tools for flatten layer

---
+++ setup.py
@@ -0,0 +1,21 @@
+"""
+Created on Nov 20, 2017
+
+Setup script for PyTorch flatten layer
+
+@author: Levan Tsinadze
+"""
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pytorch-flatten',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Levan Tsinadze',
+      author_email='levantsinadze@gmail.com',
+      url='https://github.com/levants/pytorch-flatten-layer/',
+      packages=['fllayer'],
+     )
+
